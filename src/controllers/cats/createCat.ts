cd0a915c98d03b302bbb27cb0749ad142e2b234b
Bakani Pilime <bzmpilime@gmail.com>
2019-04-04 21:22:21 +0200

    Check if a cat with similar exists

--- src/controllers/cats/createCat.ts
+++ src/controllers/cats/createCat.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
-import { missingPars, response } from "../../helpers";
+import { missingPars, response, getCatByName } from "../../helpers";
 import { logger } from "../../logger";
 import { Cat } from "../../interfaces";
 
-export default (req: Request, res: Response) => {
+export default async (req: Request, res: Response) => {
     const tags = ['create-cat'];
     const requiredPars = ['name', 'description'];
     const missing = missingPars(requiredPars, req.body); // returns an array of missing parameters from req.body
@@ -15,33 +15,47 @@ export default (req: Request, res: Response) => {
         // request body is ok
         const { name, description } = req.body;
 
+        // check if a similar cat doesn't already exist.
+
         try {
-            setTimeout(() => {
-                const id = '53289018901201239012' // auto generated id e.g database id.
+            const similarCat = await getCatByName(name);
 
-                const newCat: Cat = {
-                    id,
-                    name,
-                    description,
+            if (similarCat) {
+                res.status(400).json(response(true, "SIMILAR CAT EXISTS."));
+                logger.error(`Similar cat exists with name ${name} when trying to create cat.`, { tags: [...tags, 'similar-cat-exists']});
+            } else {
+                try {
+                    setTimeout(() => {
+                        const id = '53289018901201239012' // auto generated id e.g database id.
+
+                        const newCat: Cat = {
+                            id,
+                            name,
+                            description,
+                        }
+
+                        // add optional age
+                        if (req.body.age !== null) {
+                            newCat.age = parseInt(req.body.age);
+                        }
+
+                        // save to database
+
+                        // respond
+                        res.json(response(true, "CAT CREATED.", newCat));
+                        logger.info(`New Cat ${newCat.name} created with ID ${newCat.id}`, { tags: [...tags, 'cat-created'] })
+
+
+                    }, Math.random() * 1000)
+
+                } catch (e) {
+                    logger.error(`Exception when creating cat, e: ${e}`, { tags: [...tags, 'exception', 'createCat-exception'] });
+                    res.status(500).json(response(false, "FAILED TO CREATE CAT."));
                 }
-
-                // add optional age
-                if (req.body.age !== null) {
-                    newCat.age = parseInt(req.body.age);
-                }
-
-                // save to database
-
-                // respond
-                res.json(response(true, "CAT CREATED.", newCat));
-                logger.info(`New Cat ${newCat.name} created with ID ${newCat.id}`, { tags: [...tags, 'cat-created'] })
-
-
-            }, Math.random() * 1000)
-
+            }
         } catch (e) {
-            logger.error(`Exception when creating cat, e: ${e}`, { tags: [...tags, 'exception', 'createCat-exception']});
-            res.status(500).json(response(false, "FAILED TO CREATE CAT."));
+
         }
+
     }
 }
