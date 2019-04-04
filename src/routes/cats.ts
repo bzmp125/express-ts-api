ff773abbf44d558f2891b4354d31653ba8f6d8d5
Bakani Pilime <bzmpilime@gmail.com>
2019-04-04 18:41:59 +0200

    Add POST / route

--- src/routes/cats.ts
+++ src/routes/cats.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { getCats, getCat } from '../controllers';
+import { getCats, getCat, createCat } from '../controllers';
 
 const router: Router = Router();
 
 router.get('/', getCats);
 
 router.get('/:catId', getCat);
+
+router.post('/', createCat);
 
 export default router;
